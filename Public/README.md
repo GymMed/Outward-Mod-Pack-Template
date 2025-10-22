c0cbcf86bd584dbd881c2eba71054da99f10b52e
Gytis <medelis.gytis@gmail.com>
2025-10-22 09:32:00 +0300

    initial commit

---
+++ Public/README.md
@@ -0,0 +1,30 @@
+<h1 align="center">
+    Outward Mod Pack Template
+</h1>
+<br/>
+<div align="center">
+  <img src="https://raw.githubusercontent.com/GymMed/Outward-Mod-Pack-Template/refs/heads/main/preview/images/Logo.png" alt="Logo"/>
+</div>
+
+A template for creating a C# Outward Mod.
+
+## How to use
+
+1. Either clone/download the repository with Git or GitHub Desktop, or simply download the code manually.
+2. Open `src/OutwardModPackTemplate.sln` with any C# IDE (Visual Studio, Rider, etc)
+3. Open the `Plugin.cs` file and follow the instructions
+4. When you're ready, build the solution. It will be built to the `Release` folder (next to the `src` folder).
+5. Take the DLL from the `Release` folder and put it in the `BepInEx/plugins/` folder. If you use r2modman, this can be found by going into r2modman settings and clicking on `Browse Profile Folder`.
+
+## Tip
+
+Place automatically `.dll` document to your thunderstore profile for quicker testing.
+Inside `src/OutwardModPackTemplate.csproj` and in `Project` tag you can add:
+
+<pre><code>&lt;Target Name="PostBuild" AfterTargets="PostBuildEvent"&gt;
+  &lt;Exec Command="call &amp;quot;$(ProjectDir)..\Public\placeBuild.bat&amp;quot;" /&gt;
+&lt;/Target&gt;</code></pre>
+
+Make sure that you changed variables inside `..\Public\placeBuild.bat`.
+
+For further help, see the [Outward Modding Wiki](https://outward.fandom.com/wiki/Getting_Started_Developing_Mods), or join the [Outward Modding Discord](https://discord.gg/zKyfGmy7TR).
---
+++ src/OutwardModPackTemplate.cs
@@ -0,0 +1,141 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using SideLoader;
+using OutwardModsCommunicator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.IO;
+using OutwardModsCommunicator.EventBus;
+
+// RENAME 'OutwardModPackTemplate' TO SOMETHING ELSE
+namespace OutwardModPackTemplate
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    // if other mod pack is overwritting configs values you would add dependency to load your mod after it, to overwrite
+    // soft dependency means that your mod can work without it and should be loaded even if dependency is missing
+    // you can do the same through manifest.json(look into thunderstore documentation) and just delete this line if you like
+    [BepInDependency(OutwardModsCommunicator.OMC.GUID, BepInDependency.DependencyFlags.SoftDependency)]
+    public class OutwardModPackTemplate : BaseUnityPlugin
+    {
+        // Choose a GUID for your project. Change "myname" and "mymodpack".
+        public const string GUID = "myname.mymodpack";
+        // Choose a NAME for your project, generally the same as your Assembly Name.
+        public const string NAME = "My Mod Pack";
+        // Increment the VERSION when you release a new version of your mod.
+        public const string VERSION = "0.0.1";
+
+        // Choose prefix for log messages for quicker search and readablity
+        public static string prefix = "[MyModPack]";
+
+        internal static ManualLogSource Log;
+
+        // If you need settings, define them like so:
+        //public static ConfigEntry<bool> ExampleConfig;
+        public static ConfigEntry<bool> IsMainModPack;
+
+        // Awake is called when your plugin is created. Use this to set up your mod.
+        internal void Awake()
+        {
+            // You can find BepInEx logs in directory "BepInEx\LogOutput.log"
+            Log = this.Logger;
+            LogMessage($"Hello world from {NAME} {VERSION}!");
+
+            // Any config settings you define should be set up like this:
+            //ExampleConfig = Config.Bind("ExampleCategory", "ExampleSetting", false, "This is an example setting.");
+
+            // This is why dependency would be needed
+            // Overwrite runs post/after 'ResourcesPrefabManager' class 'Load' method
+            // You can delete this if you only want to use mods communication and don't need config overwrites
+            OutwardModsCommunicator.OMC.xmlFilePath = Path.Combine(GetProjectLocation(), "MyModsOverrides.xml");
+
+            // Harmony is for patching methods. If you're not patching anything, you can comment-out or delete this line.
+            new Harmony(GUID).PatchAll();
+
+            // Listen to other mod event and execute function.
+            // If you don't want to create function and listen to other mod you can delete this and OnTryEnchant method.
+            EventBus.Subscribe("gymmed.outward_game_settings", "EnchantmentMenu@TryEnchant", OnTryEnchant);
+        }
+
+        private static void OnTryEnchant(EventPayload payload)
+        {
+            if (payload == null) return;
+
+            // try to retrieve passed event data
+            EnchantmentMenu menu = payload.Get<EnchantmentMenu>("menu", null);
+
+            // if event data is null log and stop execution
+            if (menu == null)
+            {
+                LogSL("Mod gymmed.outward_game_settings event EnchantmentMenu@TryEnchant returned null for EnchantmentMenu");
+                return;
+            }
+
+            // Lets log success
+            LogSL($"{GUID} successfully communicated with gymmed.outward_game_settings mod and passed menu!");
+
+            // Get currently possible enchantmentID from EnchantmentMenu
+            int enchantmentID = menu.GetEnchantmentID();
+
+            // if enchatment couldn't be determined stop execution
+            if (enchantmentID == -1)
+                return;
+
+            // Retrieve enchantment
+            Enchantment enchantment = ResourcesPrefabManager.Instance.GetEnchantmentPrefab(enchantmentID);
+
+            // Assume that there is possibility that retrieve failed. Stop Code Execution
+            if (enchantment == null)
+                return;
+
+            LogSL($"{enchantment.Name} tried to be applied!");
+        }
+
+        // Update is called once per frame. Use this only if needed.
+        // You also have all other MonoBehaviour methods available (OnGUI, etc)
+        internal void Update()
+        {
+        }
+
+        //  Log message with prefix
+        public static void LogMessage(string message)
+        {
+            Log.LogMessage($"[{OutwardModPackTemplate.prefix}] {message}");
+        }
+
+        // Log message through side loader, helps to see it
+        // if you are using UnityExplorer and want to see live logs
+        public static void LogSL(string message)
+        {
+            SL.Log($"[{OutwardModPackTemplate.prefix}] {message}");
+        }
+
+        // Gets mod dll location
+        public static string GetProjectLocation()
+        {
+            return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        // This is an example of a Harmony patch.
+        // If you're not using this, you should delete it.
+        [HarmonyPatch(typeof(ResourcesPrefabManager), nameof(ResourcesPrefabManager.Load))]
+        public class ResourcesPrefabManager_Load
+        {
+            static void Postfix(ResourcesPrefabManager __instance)
+            {
+                // use Debug build for things you don't want to release
+                #if DEBUG
+                // provide class and method separated by @ for easier live debugging
+                LogSL("ResourcesPrefabManager@Load called!");
+                #endif
+
+                // Log all registered events
+                EventBusDataPresenter.LogRegisteredEvents();
+            }
+        }
+    }
+}
